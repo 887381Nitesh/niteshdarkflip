f4256c5822364c626e19fe76f6e5d90e0a9e7eb4
887381Nitesh <kumarnitesh597@gmail.com>
2023-05-11 18:23:51 +0530

    first commit

--- script.js
+++ script.js
@@ -42,15 +42,32 @@ popupCloseButton.addEventListener('click',()=>{
     document.querySelector('#main').style.display='none';
 })
 
-/*
-function loadCoupon()
-{
-    document.getElementById('coupon').style.visibility='visible';
-    document.getElementById('main').style.opacity='0.7'
+// geo location
+let x = document.getElementById('out');
+let y = document.getElementById('weather');
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText="Geo Not Supported"
+    }
 }
-function closeCoupon()
-{
-    document.getElementById('coupon').style.visibility='hidden';
-    document.getElementById('main').style.opacity='1'
+
+function showPosition(data){
+    console.log(data)
+    let lat = data.coords.latitude
+    let lon = data.coords.longitude
+    x.innerText = `Lat ${lat} and long ${lon}`
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+    //api calling
+    fetch(url,{method: 'GET'})
+    //return promise
+    .then((res) =>res.json())
+    //resolve the promise
+    .then((data) => {
+        console.log(data)
+        let cityName = data.city.name;
+        let temp = data.list[0].temp.day+" °C"
+        y.innerText = ` ${cityName} is ${temp}`
+    })
 }
-*/
